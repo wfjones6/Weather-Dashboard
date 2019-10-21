f8b04fcc17b8f84496b4f6349f35d5315a551f64
wfjones6 <55958934+wfjones6@users.noreply.github.com>
2019-10-20 22:16:06 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,32 @@
-  var cityID = 6167865;
-  var key = 'fe692e6f7fc6afc8b2dde67841dbabe0';
-  var url = 'https://api.openweathermap.org/data/2.5/weather?id='+cityID+'&appid='+key;
+var cityID = 6167865;
+var key = 'fe692e6f7fc6afc8b2dde67841dbabe0';
+var url = 'https://api.openweathermap.org/data/2.5/weather?id='+cityID+'&appid='+key;
+var curDate = new Date();
+var nDate = curDate.toDateString();
 
-  var xhr = new XMLHttpRequest();	  // Create XMLHttpRequest object
-  
-  xhr.onload = function()			      // When readystate changes
+var xhr = new XMLHttpRequest();	// Create XMLHttpRequest object
+
+xhr.onload = function()			// When readystate changes
+{
+  if(xhr.status === 200)			//If server status was ok
   {
-  	if(xhr.status === 200)			    //If server status was ok
-    {
-    	responseObject = JSON.parse(xhr.responseText);
-        
-      // Build up string with new content (could also use DOM manipulation)
-      var curDate = new Date();
-      var nDate = curDate.toDateString();
-      var fahrenheit = Math.round(((parseFloat(responseObject.main.temp)-273.15)*1.8)+32);
-      var newContent = "";
+    responseObject = JSON.parse(xhr.responseText);
 
-      newContent += '<div >';
-      newContent += '<h3>' + responseObject.name + ' (' + nDate + ')</h3>';
-      newContent += '<p>Temperature: ' + fahrenheit + ' &degF</p>';
-      newContent += '<p>Humidity: ' + responseObject.main.humidity + '%</p>';
-      newContent += '<p>Wind Speed: ' + responseObject.wind.speed + ' MPH</p>';;
-      newContent += '</div>';
+      // Build up string with new content (could also use DOM manipulation)
+      var fahrenheit = Math.round(((parseFloat(responseObject.main.temp)-273.15)*1.8)+32);
+      var curConditions = "";
+      curConditions += '<div >';
+      curConditions += '<h3>' + responseObject.name + ' (' + nDate + ')</h3>';
+      curConditions += '<p>Temperature: ' + fahrenheit + ' &degF</p>';
+      curConditions += '<p>Humidity: ' + responseObject.main.humidity + '%</p>';
+      curConditions += '<p>Wind Speed: ' + responseObject.wind.speed + ' MPH</p>';
+      curConditions += '<p>UV Index: '+ 10.4 + '</p>';
+      curConditions += '</div>';
 
       //update the page with the new content
-      document.getElementById('description').innerHTML = newContent;
-    }
-  };
-  
-  xhr.open('GET', url, true);	  // prepare the request
-  xhr.send(null);				        // send the request
+      document.getElementById('curConditions').innerHTML = curConditions;
+  }
+};
 
+xhr.open('GET', url, true);	// prepare the request
+xhr.send(null);
