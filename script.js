1217bd7a4a674a1aa51612d984772fe7347bf406
wfjones6 <55958934+wfjones6@users.noreply.github.com>
2019-10-21 20:43:30 -0500

    Update script.js

--- script.js
+++ script.js
@@ -5,67 +5,31 @@ var cityName = '';
 var url = '';
 var curDate = new Date();
 var nDate = curDate.toDateString();
-var lat;
-var lon;
 
-function getCurrent(curURL)
+var xhr = new XMLHttpRequest();	// Create XMLHttpRequest object
+
+xhr.onload = function()			// When readystate changes
 {
-  var xhr = new XMLHttpRequest();	// Create XMLHttpRequest object
-  xhr.onreadystatechange = function()
+  if(xhr.status === 200)			//If server status was ok
   {
-        if(this.readyState == 4 && this.status == 200) 
-	{
-	    responseObject = JSON.parse(xhr.responseText);
+    responseObject = JSON.parse(xhr.responseText);
 
-	    // Build up string with new content (could also use DOM manipulation)
-	    var fahrenheit = Math.round(((parseFloat(responseObject.main.temp)-273.15)*1.8)+32);
-	    var curConditions = "";
+      // Build up string with new content (could also use DOM manipulation)
+      var fahrenheit = Math.round(((parseFloat(responseObject.main.temp)-273.15)*1.8)+32);
+      var curConditions = "";
+	  
+      curConditions += '<div >';
+      curConditions += '<h3>' + responseObject.name + ' (' + nDate + ')</h3>';
+      curConditions += '<p>Temperature: ' + fahrenheit + ' &degF</p>';
+      curConditions += '<p>Humidity: ' + responseObject.main.humidity + '%</p>';
+      curConditions += '<p>Wind Speed: ' + responseObject.wind.speed + ' MPH</p>';
+      curConditions += '<p>UV Index: '+ 10.4 + '</p>';
+      curConditions += '</div>';
 
-	    curConditions += '<div >';
-	    curConditions += '<h3>' + responseObject.name + ' (' + nDate + ')</h3>';
-	    curConditions += '<p>Temperature: ' + fahrenheit + ' &degF</p>';
-	    curConditions += '<p>Humidity: ' + responseObject.main.humidity + '%</p>';
-	    curConditions += '<p>Wind Speed: ' + responseObject.wind.speed + ' MPH</p>';
-	    curConditions += '<p>UV Index: '+ 10.4 + '</p>';
-	    curConditions += '</div>';
-
-	    //Get latitude and longitude needed for UV Index
-	    lat = responseObject.coord.lat;
-	    lon = responseObject.coord.lon;
-
-   	    //update the page with the new content
-	    document.getElementById('curConditions').innerHTML = curConditions;
-	}
+      //update the page with the new content
+      document.getElementById('curConditions').innerHTML = curConditions;
   }
-  xhr.open('GET', url, true);	// prepare the request
-  xhr.send(null);		// send the request
-}
-
-function getUVIndex(uvindexURL)
-{
-  var xhr = new XMLHttpRequest();	// Create XMLHttpRequest object
-  xhr.open('GET', url, true);	// prepare the request
-  xhr.onreadystatechange = function()
-  {
-        if(xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) 
-	{
-	    responseObject = JSON.parse(xhr.responseText);
-
-	    // Build up string with new content (could also use DOM manipulation)
-	    var fahrenheit = Math.round(((parseFloat(responseObject.main.temp)-273.15)*1.8)+32);
-	    var curConditions = "";
-
-	    curConditions += '<div >';
-	    curConditions += '<p>UV Index: '+ responseObject.value + '</p>';
-	    curConditions += '</div>';
-
-	    //update the page with the new content
-	    document.getElementById('uvIndex').innerHTML = curConditions;
-	}
-  }
-  xhr.send(null);		// send the request
-}
-
+};
 
 // Search
 searchBtn.addEventListener('click', function () 
@@ -74,11 +38,7 @@ searchBtn.addEventListener('click', function ()
   
   // Current conditions URL
   url = 'https://api.openweathermap.org/data/2.5/weather?q='+cityName+'&appid='+key;
-  getCurrent(url);
 
-  if (lat != 0 && lon != 0)
-  {
-  	url = 'http://api.openweathermap.org/data/2.5/uvi?appid='+key+'&lat='+lat+'&lon='+lon';
-  	getUVIndex(url);
-  }
+  xhr.open('GET', url, true);	// prepare the request
+  xhr.send(null);		// send the request
 })
