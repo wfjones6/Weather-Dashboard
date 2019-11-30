b61059bed0f7b0dd179360c282874d5938f77e3c
wfjones6 <55958934+wfjones6@users.noreply.github.com>
2019-11-29 23:23:40 -0600

    Update script.js

--- script.js
+++ script.js
@@ -13,8 +13,7 @@ xhr.onload = function()			// When readystate changes
   if(xhr.status === 200)			//If server status was ok
   {
     responseObject = JSON.parse(xhr.responseText);
-    var url2 = 'http://api.openweathermap.org/data/2.5/uvi?appid='+key+'&lat='+responseObject.coord.lat+'&lon='+responseObject.coord.lon;
-	  
+  
 
       // Build up string with new content (could also use DOM manipulation)
       var curConditions = "";
