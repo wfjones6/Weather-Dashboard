8676a8766aaa9dc3ca503a832f6aaf942ab6e377
wfjones6 <55958934+wfjones6@users.noreply.github.com>
2019-12-01 22:42:29 -0600

    Update script.js

--- script.js
+++ script.js
@@ -89,7 +89,28 @@ function get5Day()
 		if (xhr3.status === 200 || xhr3.status === 0)//If server status was ok
 		{
 		    responseObject3 = JSON.parse(xhr3.responseText);
-		    // Build up string with new content (could also use DOM manipulation)
+		    var iconId = responseObject3.list[0].weather[0].icon;
+		    var iconUrl = 'http://openweathermap.org/img/wn/'+iconId+'@2x.png';
+		    var dayArray = [];
+		    var forecastArray = [];
+
+		    // The forecast list includes weather data every 3 hours.
+		    // To get one forecast every 24 hours, we need every 8th record
+		    // because 24 / 3 = 8 entries per day. We only want one per day.
+		    for (i = 0; i < responseObject3.list.length; i+=8)
+		    {
+			iconId = responseObject3.list[i].weather[0].icon;
+			iconUrl = 'http://openweathermap.org/img/wn/'+iconId+'@2x.png';
+
+			dayArray = [responseObject3.list[i].dt_txt,
+							iconUrl,
+					responseObject3.list[i].main.temp,
+					responseObject3.list[i].main.humidity];
+
+		       forecastArray.push(dayArray);
+		    }
+
+  		    // Build up string with new content (could also use DOM manipulation)
 		    var curConditions = "";
 
 		    curConditions += '<div>';
@@ -98,6 +119,17 @@ function get5Day()
 		    curConditions += '<p>Humidity: ' + mHumidity + '%</p>';
 		    curConditions += '<p>Wind Speed: ' + mWindSp + ' MPH</p>';
 		    curConditions += '<p>UV Index: '+ mUV + '</p>';
+		    curConditions += '<h4>5-Day Forecast:</h4>';
+
+		    for (i = 0; i < forecastArray.length; i++)
+		    {
+		      curConditions += '<p>Day-'+ (i+1) +' Date: '+ forecastArray[i][0] + '</p>';
+		      curConditions += '<p>Icon URL: '+ forecastArray[i][1] + '</p>';
+		      curConditions += '<p>Temp: '+ forecastArray[i][2] + ' &degF</p>';
+		      curConditions += '<p>Humidity: '+ forecastArray[i][3] + '%</p>';
+		      curConditions += '</br>';
+		    }
+
 		    curConditions += '</div>';
 
 		    //update the page with the new content
