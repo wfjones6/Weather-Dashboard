9dcc5d84258a6a7c99e340bc093615bdb0b9383f
wfjones6 <55958934+wfjones6@users.noreply.github.com>
2019-12-14 14:01:06 -0600

    Create searchHistory.js

---
+++ searchHistory.js
@@ -0,0 +1,29 @@
+var searchedItems = [
+    {
+        item: "1",
+    },
+    {
+        item: "2",
+    },
+    {
+        item: "3",
+    },
+    {
+        item: "4",
+    },
+    {
+        item: "5",
+    },
+    {
+        item: "6",
+    },
+    {
+        item: "7",
+    },
+    {
+        item: "8",
+    },
+    {
+        item: "9",
+    }
+];
